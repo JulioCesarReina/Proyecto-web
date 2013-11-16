11483657def34155e2ca71e1e571fc834377aa6c
nicomartinez <juannicolas.martinez@uptc.edu.co>
2013-11-16 14:59:54 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,17 @@
 #Archivo en Donde se encuentra la parte logica de la pagina
+def add_person(firstName, lastName, cc, phone, priority, time_of_admission):
+  print ""
+  
+def show_list():
+  print ""
+  
+def delete_person():
+  print ""
+  
+def update_person():
+  print ""
+  
+
+  
+  
+  
