86714cd578645fb530d320da7ac8161ce80c953a
nicomartinez <juannicolas.martinez@uptc.edu.co>
2013-11-19 18:45:30 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,4 +1,13 @@
 #Archivo en Donde se encuentra la parte logica de la pagina
+import MySQLdb
+
+try:
+  db = MySQLdb.connect(host = "localhost", user = "root", passwd = "", db = "PatientRegistrydb")
+  
+  con = db.cursor()
+  
+  cur.execute("")
+
 def add_person(firstName, lastName, cc, phone, priority, time_of_admission):
   print ""
   
