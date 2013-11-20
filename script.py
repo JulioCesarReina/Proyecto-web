3918a95ef695a76060321a552ac4f7866c19f6b4
nicomartinez <juannicolas.martinez@uptc.edu.co>
2013-11-19 20:30:29 -0500

    Update script.py

--- script.py
+++ script.py
@@ -8,24 +8,27 @@ try:
   
   con.execute("CREATE TABLE IF NOT EXISTS patientRegistry (id INT PRYMARY KEY AUTO_INCREMENT, fisrtName varchar(30),lastname varchar(30), cc INT, phone int, priority varchar(15), time_of_admission Timestamp)")
   
+  def add_person(firstName, lastName, cc, phone, priority, time_of_admission):
+    con.execute("INSERT INTO patientRegistry VALUES(" + firstname + ", " + lastname + ", " + cc + ", " + phone + ", " + priority + ", " + time_of_admission +  ")")
+    print ""
+  
+  def show_list():
+    con.execute("SELECT * FROM patientRegistry")
+    print ""
+  
+  def delete_person():
+    con.execute()
+    con.commit()
+    print ""
+  
+  def update_person(firstName, lastName, cc, phone, priority, time_of_admission):
+    con.execute("UPDATE")
+    con.commit()
+    print ""
+  
 except MySQLdb, e:
   print "No se pudo realizar conexion con la base de datos"
 
-def add_person(firstName, lastName, cc, phone, priority, time_of_admission):
-  con.execute("INSERT INTO patientRegistry VALUES(" + firstname + ", " + lastname + ", " + cc + ", " + phone + ", " + priority + ", " + time_of_admission +  ")")
-  print ""
-  
-def show_list():
-  con.execute("SELECT * FROM patientRegistry")
-  print ""
-  
-def delete_person():
-  con.execute()
-  print ""
-  
-def update_person(firstName, lastName, cc, phone, priority, time_of_admission):
-  con.execute("UPDATE")
-  print ""
   
 
   
