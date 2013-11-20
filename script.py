801df99f90dbd79cec0a504ad2f13898cf4da0c4
nicomartinez <juannicolas.martinez@uptc.edu.co>
2013-11-19 19:34:46 -0500

    Update script.py

--- script.py
+++ script.py
@@ -6,21 +6,25 @@ try:
   
   con = db.cursor()
   
-  cur.execute("CREATE TABLE IF NOT EXISTS patientRegistry (id INT PRYMARY KEY AUTO_INCREMENT, fisrtName varchar(30),lastname varchar(30), cc INT, priority varchar(15
+  con.execute("CREATE TABLE IF NOT EXISTS patientRegistry (id INT PRYMARY KEY AUTO_INCREMENT, fisrtName varchar(30),lastname varchar(30), cc INT, phone int, priority varchar(15), time_of_admission Timestamp)")
   
 except MySQLdb, e:
   print "No se pudo realizar conexion con la base de datos"
 
 def add_person(firstName, lastName, cc, phone, priority, time_of_admission):
+  con.execute("INSERT INTO patientRegistry(" + firstname + ", " + lastname + ", " + cc + ", " + phone + ", " + priority + ", " + time_of_admission +  ")")
   print ""
   
 def show_list():
+  con.execute("SELECT * FROM patientRegistry")
   print ""
   
 def delete_person():
+  con.execute()
   print ""
   
-def update_person():
+def update_person(firstName, lastName, cc, phone, priority, time_of_admission):
+  con.execute("UPDATE")
   print ""
   
 
